b7cd74972688517bcdbcdac0ff33d995811c3d3c
one person in the universe <tokkivanc13@gmail.com>
2025-03-01 12:02:54 +0300

    increase precision on remaining constants

---
+++ newtonsmethod_W1.jl
@@ -0,0 +1,13 @@
+setprecision(510; base=10)
+f(z::Number) = z*exp(z)-1
+deriv(f::Function, h::Real = 1e-12) = x -> (f(x+h)-f(x))/h
+iterate(f::Function, s::Number, h::Real = 1e-12) = s - f(s)/deriv(f, h)(s)
+seed = big(2)
+prev = Inf
+while true
+    println(seed)
+    global seed = iterate(f, seed, big(10.)^-240)
+    if seed == prev || (isnan(seed) && isnan(seed)) break end
+    global prev = seed
+end
+println(seed)
